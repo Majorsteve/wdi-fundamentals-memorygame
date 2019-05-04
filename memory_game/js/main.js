f452652dbde25024432446a7d33f883d67509c15
stevekim <stevekim@stevekims-Air.fios-router.home>
2019-05-04 15:56:12 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -8,28 +8,37 @@ var cardFour = "king";
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
-*/
-
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
 
 var cardOne = cards[0];
 cardsInPlay.push(cardOne);
-console.log("User flipped queen");
+console.log("User flipped" + cardOne);
 
 var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
-console.log("User flipped king");
+console.log("User flipped " + cardTwo);
+*/
 
-if (cardsInPlay.length === 2) {
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!"); }
-else { 
-	alert ("Sorry, try again."); }
- }
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = []
+
+function checkForMatch(){
+	if (cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+  			console.log("You found a match!");}
+		else {
+  	console.log("Sorry, try again.");
+}
+}
+};
+
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
 
 
+};
 
-
-
-
+flipCard(0);
+flipCard(2);
